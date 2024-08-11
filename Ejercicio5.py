81399f607f6f814041b94a02f5eb41648c04b89a
ChristianCP1104 <christian.costilla@unmsm.edu.pe>
2024-08-11 16:46:43 +0000

    Subiendo Ejercicio5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,4 +1,4 @@
-# Paso 1: Solicitar la cadena de texto
+# Paso 1: Solicitar cadena de texto
 texto = input("Ingrese una cadena de texto: ")
 
 # Paso 2: Contar el número de palabras
@@ -7,6 +7,6 @@ numero_palabras = len(texto.split())
 # Paso 3: Contar el número de caracteres
 numero_caracteres = len(texto)
 
-# Paso 4: Imprimir los resultados
+# Paso 4: Resultados
 print(f"Número de palabras: {numero_palabras}")
 print(f"Número de caracteres: {numero_caracteres}")
